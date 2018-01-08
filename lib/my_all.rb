bd743c2de694bb4f4440824827d53414b23abb0c
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-08 19:58:35 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,8 +4,10 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
+    binding.pry
     block_return_values << yield(collection[i])
     i = i + 1
+    binding.pry
   end
 
   if block_return_values.include?(false)
